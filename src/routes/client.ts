ff820e8a0c1f2d9f180c502d21613eddae411adb
cheikh ibrahima dieng <barhamadieng66@gmail.com>
2024-10-28 08:46:58 +0000

    feat(parametre): liste des agences, le plafond de l'utilisateur, changer le mot de passe de l'utilisateur

--- src/routes/client.ts
+++ src/routes/client.ts
@@ -1,11 +1,16 @@
 import express from "express";
 import clientController from "../controller/ClientController.js";
 import { isAuthenticatedGlobal } from "../middleware/auth.js";
-//
+
 const router = express.Router();
-//
 
-router.use(isAuthenticatedGlobal); // Utilisez le middleware pour toutes les routes
+// router.use(isAuthenticatedGlobal);
 
+
+router.route("/agences").get(clientController.getAllAgence);
+
+router.route("/plafonds").get(clientController.getUserPlafond);
+
+router.route("/password").put(clientController.changePassword);
 
 export { router };
